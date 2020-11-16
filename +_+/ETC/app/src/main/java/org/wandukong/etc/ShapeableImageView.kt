1d2eb220941b9083ce29832d838eb263bd7c6cbe
wandukong <seungwan37@naver.com>
2020-11-16 16:32:26 +0900

    ShapeableImageView

---
+++ +_+/ETC/app/src/main/java/org/wandukong/etc/ShapeableImageView.kt
@@ -0,0 +1,11 @@
+package org.wandukong.etc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShapeableImageView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shapeable_image_view)
+    }
+}
