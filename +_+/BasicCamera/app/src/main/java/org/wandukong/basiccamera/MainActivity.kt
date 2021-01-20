5dbf0031bb63f79e35b93ea31b7e4a896bfbf179
wandukong <seungwan37@naver.com>
2021-01-20 18:31:24 +0900

    [ADD] camera capture for loading ans saving picture

---
+++ +_+/BasicCamera/app/src/main/java/org/wandukong/basiccamera/MainActivity.kt
@@ -0,0 +1,140 @@
+package org.wandukong.basiccamera
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.core.net.toUri
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var currentPhotoPath: String
+    private lateinit var outputDirectory: File
+
+    companion object{
+        const val REQUEST_TAKE_PHOTO = 1
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_camera.setOnClickListener {
+            picturesFromCamera()
+        }
+    }
+
+    private fun picturesFromCamera() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ContextCompat.checkSelfPermission(
+                    baseContext,
+                    Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_DENIED){
+                // permission denied
+                val permissions = arrayOf(android.Manifest.permission.CAMERA)
+                // show popup to request runtime permission
+                requestPermissions(permissions, REQUEST_TAKE_PHOTO)
+            }else{
+                takePictureIntent()
+            }
+        }else{
+            // OS is < Marshmallow
+            takePictureIntent()
+        }
+    }
+
+    private fun takePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    Log.e("Error", ex.toString())
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "org.wandukong.basiccamera",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            REQUEST_TAKE_PHOTO -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePictureIntent()
+                } else { // permission from popup denied
+                    Toast.makeText(baseContext, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            iv_camera.setImageURI(currentPhotoPath.toUri())
+            galleryAddPic()
+        }
+    }
+
+    private fun galleryAddPic() {
+        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
+            val file = File(currentPhotoPath)
+            mediaScanIntent.data = Uri.fromFile(file)
+            sendBroadcast(mediaScanIntent)
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getOutputDirectory()
+        return File.createTempFile(
+            timeStamp, /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply {mkdirs()}
+        }
+        return if(mediaDir != null && mediaDir.exists())
+            mediaDir
+        else
+            filesDir
+    }
+}
