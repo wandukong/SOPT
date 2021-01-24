3360ee22e8c446a44a78e537e38643dd0d5843bf
wandukong <seungwan37@naver.com>
2021-01-24 14:15:48 +0900

    [FEAT] added jetpack navigation

---
+++ +_+/Navigation/app/src/main/java/org/wandukong/navigation/FirstFragment.kt
@@ -0,0 +1,36 @@
+package org.wandukong.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import org.wandukong.navigation.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    lateinit var binding : FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            moveFragment()
+        }
+    }
+
+    fun moveFragment(){
+        val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("Hello!!!")
+        findNavController().navigate(action)
+    }
+}
