29f26d5d6679dbcd8cc6d9b37ecf64f221d57bea
wandukong <seungwan37@naver.com>
2021-02-03 17:59:10 +0900

    [FEAT] applied savedStateHandle

--- +_+/SavedStateHandle/app/src/main/java/org/wandukong/savedstatehandle/MainViewModel.kt
+++ +_+/SavedStateHandle/app/src/main/java/org/wandukong/savedstatehandle/MainViewModel.kt
@@ -1,18 +1,26 @@
 package org.wandukong.savedstatehandle
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-    private val _count = MutableLiveData<Int>(0)
-    val count: LiveData<Int> get() = _count
+class MainViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+    private var savedCount = savedStateHandle.get<Int>("count") ?: 0
+        set(value) {
+            savedStateHandle.set("count", value)
+            field = value
+            _count.value = savedCount
+        }
+    private val _count = MutableLiveData<Int>(savedCount)
+    val count : LiveData<Int> get() = _count
 
     fun plusCount(){
-        _count.value = _count.value!!.plus(1)
+        savedCount++
     }
 
     fun minusCount(){
-        _count.value = _count.value!!.minus(1)
+        savedCount--
     }
 }
